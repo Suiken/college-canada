8781d1c4e4ef71d08ea972d1cd2625566674003f
Suy <kevin.suy@cgi.com>
2016-10-17 17:49:21 -0400

    add partner class (not finished)

---
+++ client/app/partner.ts
@@ -0,0 +1,10 @@
+export class Partner {
+  constructor(
+    public id: number,
+    public name: string,
+	public address: string,
+	public city: string,
+	public province: string,
+	public postalCode: string,
+	public country: string){ }
+}
